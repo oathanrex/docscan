614035ebf890e62b77bfaa2fe306edef67477c28
Oathan Rex <90545164+oathanrex@users.noreply.github.com>
2026-03-10 22:17:49 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,10 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://doc-scan-psi.vercel.app",
-        "http://localhost:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
